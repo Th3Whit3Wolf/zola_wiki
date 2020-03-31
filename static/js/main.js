97275a42229536011861d95b278b9f7958901b0e
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2020-03-31 21:14:55 +0100

    Added dropdown menu support

--- static/js/main.js
+++ static/js/main.js
@@ -20,47 +20,45 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 );
 
-// dropdown menu
-document.addEventListener('DOMContentLoaded', function () {
+// Dropdowns
 
-    // Dropdowns
-    var $selects = getAll('.select');
-    if ($selects.length > 0) {
-        $selects.forEach(function ($el) {
-            $el.addEventListener('click', function (event) {
-                event.stopPropagation();
-                $el.classList.toggle('is-active');
-                //const selection = ['kernel', 'arch', 'release', 'repo'];
-                var kernel = showSelected('kernel');
-                var arch = showSelected('arch');
-                var repo = showSelected('repo');
-                var release = showSelected('release');
-                var q = getQuery();
-                console.log(q);
-                var link = kernel + arch + release + repo + q;
-                var link = link.slice(0, -1);
-                document.getElementById("searchBtn").setAttribute("href", "/packages?" + link);
-                console.log(link);
-            });
+var $dropdowns = getAll('.dropdown:not(.is-hoverable)');
+
+if ($dropdowns.length > 0) {
+    $dropdowns.forEach(function ($el) {
+        $el.addEventListener('click', function (event) {
+            event.stopPropagation();
+            $el.classList.toggle('is-active');
         });
-    }
+    });
 
-    // Functions
-    function getAll(selector) {
-        return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
-    }
+    document.addEventListener('click', function (event) {
+        closeDropdowns();
+    });
+}
 
-    function showSelected(item) {
-        var selector = document.getElementById(item);
-        var value = selector[selector.selectedIndex].value;
-        //console.log(value);
-        if (value === "any") {
-            return '';
-        } else {
-            return item + "=" + value + "&";
-        }
+function closeDropdowns() {
+    $dropdowns.forEach(function ($el) {
+        $el.classList.remove('is-active');
+    });
+}
+
+// Functions
+function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+}
+
+function showSelected(item) {
+    var selector = document.getElementById(item);
+    var value = selector[selector.selectedIndex].value;
+    //console.log(value);
+    if (value === "any") {
+        return '';
+    } else {
+        return item + "=" + value + "&";
     }
-});
+}
+
 
 // Get Query
 function getQuery() {
