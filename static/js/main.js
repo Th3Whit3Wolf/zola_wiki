ddbfb28c34f7bcbe496230a785ce7054a91c8cf3
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2020-03-31 10:51:00 +0100

    Added Sidebar Anchor Menu Navigation for Table of Contents

--- static/js/main.js
+++ static/js/main.js
@@ -71,3 +71,73 @@ function getQuery() {
         return "q=" + x + "&";
     }
 }
+
+// Sidebar Anchor Menu Navigation
+var quickLinks = document.querySelectorAll("#anchorMenu .menu-list a");
+var positionArray = [], hashArray = [];
+
+for (var i = 0; i < quickLinks.length; i++) {
+    hashArray.push(quickLinks[i].href.substring(quickLinks[i].href.indexOf('#') + 1));
+}
+
+for (var i = 0; i < hashArray.length; i++) {
+    positionArray.push(document.getElementById(hashArray[i]).getBoundingClientRect().top);
+}
+
+document.addEventListener("scroll", scrollHandler, true);
+
+function scrollHandler() {
+    currentScrollPosition = document.documentElement.scrollTop;
+    //currentActivePosition = document.getElementsByClassName('toc-is-active')[0].getBoundingClientRect().top;
+
+    for (var i = 1; i <= positionArray.length; i++) {
+
+        if (currentScrollPosition >= positionArray[i - 1] && currentScrollPosition < positionArray[i]) {
+
+            var currentActive = document.getElementsByClassName('toc-is-active')[0];
+
+            if (currentActive != undefined) {
+                currentActive.classList.remove('toc-is-active');
+            }
+
+            quickLinks[i - 1].classList.add('toc-is-active');
+
+            break;
+
+        }
+
+        if (currentScrollPosition > positionArray[positionArray.length - 1]) {
+            var currentActive = document.getElementsByClassName('toc-is-active');
+
+            var currentActive = document.getElementsByClassName('toc-is-active')[0];
+
+            if (currentActive != undefined) {
+                currentActive.classList.remove('toc-is-active');
+            }
+            quickLinks[quickLinks.length - 1].classList.add('toc-is-active');
+
+            break;
+
+        }
+    }
+};
+
+for (var i = 0; i < quickLinks.length; i++) {
+    quickLinks[i].addEventListener('click', function (event) {
+        document.removeEventListener('scroll', scrollHandler, true);
+
+        isActive = document.getElementsByClassName('toc-is-active')[0];
+
+        if (isActive != undefined) {
+            isActive.classList.remove('toc-is-active');
+        }
+
+        this.classList.add('toc-is-active');
+
+        setTimeout(function () {
+            document.addEventListener("scroll", scrollHandler, true);
+        }, 600);
+
+    });
+
+}
